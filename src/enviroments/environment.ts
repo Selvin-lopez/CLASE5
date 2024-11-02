248e58bbf0458debfc769fa39ee954c0ba44fb3a
selvinlopez <slopezs27@miumg.edu.gt>
2024-11-02 13:18:06 -0600

    Agregar El proyecto

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZvWOh1iPoDipmZ2boAX2PDbUXpOumI9U",
+    authDomain:  "desarrollowed-f70ae.firebaseapp.com",
+    databaseURL: "https://desarrollowed-f70ae-default-rtdb.firebaseio.com",
+    projectId: "desarrollowed-f70ae",
+    storageBucket:   "desarrollowed-f70ae.appspot.com",
+    messagingSenderId: "446142865880",
+    appId:  "1:446142865880:web:7836e47eba5b99bfa412ad"
+  }
+};
